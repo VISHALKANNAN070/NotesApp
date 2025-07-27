4a17adda409d5587cbde48ab4bbfc0c0fed77f89
VISHALKANNAN070 <vishalkannan070@gmail.com>
2025-07-27 11:56:43 +0530

    Refactor rate limiter middleware to require Google ID and improve error handling

--- server/src/middleware/rateLimiter.js
+++ server/src/middleware/rateLimiter.js
@@ -1,17 +1,23 @@
 import ratelimit from "../config/upstash.js";
 
-const rateLimiter = async(req, res, next) => {
-    try {
-        const {success} = await ratelimit.limit("my-limit-key")   
-        if(!success){
-            return res.status(429).json({ message: "Too many requests, please try again later." });
-        }
-        next()
-    } catch (error) {
-        console.error("Rate limiter error:", error);
-        res.status(500).json({ message: "Internal Server Error" });
-        next(error)
+const rateLimiter = async (req, res, next) => {
+  try {
+    const googleId = req.user?.googleId;
+    const { success } = await ratelimit.limit({ googleId });
+    if (!googleId) {
+      return res.status(400).json({ message: "Google ID is required." });
     }
-}
+    if (!success) {
+      return res
+        .status(429)
+        .json({ message: "Too many requests, please try again later." });
+    }
+    next();
+  } catch (error) {
+    console.error("Rate limiter error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
+  }
+};
 
 export default rateLimiter;
